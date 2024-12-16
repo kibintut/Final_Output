23adfa28f0e998fb908be044e959379089c22be8
Kevin Perez <perezzkevin021@gmail.com>
2024-12-16 15:29:14 +0800

    Interrgated_Final_Project

--- public/app.js
+++ public/app.js
@@ -1,9 +1,13 @@
-// Fetch data from the back-end API
-fetch("/api/message")
-  .then((response) => response.json())
-  .then((data) => {
-    document.getElementById("message").innerText = data.message;
-  })
-  .catch((error) => {
-    console.error("Error fetching API data:", error);
-  });
+// // Fetch data from the back-end API
+// fetch("/api/message")
+//   .then((response) => response.json())
+//   .then((data) => {
+//     document.getElementById("message").innerText = data.message;
+//   })
+//   .catch((error) => {
+//     console.error("Error fetching API data:", error);
+//   });
+
+const about = document.getElementById("about");
+
+function about() {}
